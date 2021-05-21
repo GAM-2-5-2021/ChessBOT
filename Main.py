402946950c3fee7bb2d9e8a3bcc6efe990a9d458
KarloZivkovic <84178721+KarloZivkovic@users.noreply.github.com>
2021-05-21 10:45:23 +0200

    legal moves checked
    how the pieces move, avoiding the chance to eat the king, when king is in check, block or move the king, checkmate, stalemate

--- Main.py
+++ Main.py
@@ -8,63 +8,103 @@ MAX_FPS = 15
 IMAGES = {}
 
 def loadImages():
-    pieces = ["wp", "wR", "wN", "wB", "wK", "wQ", "bp", "bR", "bN", "bB", "bK", "bQ"]
-    for piece in pieces:
-        IMAGES[piece] = p.transform.scale(p.image.load("images/" + piece + ".png"), (SQ_SIZE, SQ_SIZE))
-       
+	pieces = ["wp", "wR", "wN", "wB", "wK", "wQ", "bp", "bR", "bN", "bB", "bK", "bQ"]
+	for piece in pieces:
+		IMAGES[piece] = p.transform.scale(p.image.load("images/" + piece + ".png"), (SQ_SIZE, SQ_SIZE))
+	   
 def main():
-    p.init()
-    screen = p.display.set_mode((WIDTH, HEIGHT))
-    clock = p.time.Clock()
-    screen.fill(p.Color("white"))
-    gs = Engine.GameState()
-    loadImages()
-    running = True
-    sqSelected = ()
-    playerClicks = []
-    while running:
-        for i in p.event.get():
-            if i.type == p.QUIT:
-                running = False
-            elif i.type == p.MOUSEBUTTONDOWN:
-                location = p.mouse.get_pos()
-                col = location[0]//SQ_SIZE
-                row = location[1]//SQ_SIZE
-                if sqSelected == (row, col):
-                    sqSelected = ()
-                    playerClicks = []
-                else:
-                    sqSelected = (row, col)
-                    playerClicks.append(sqSelected)
-                if len(playerClicks) == 2:
-                    move = Engine.Move(playerClicks[0], playerClicks[1], gs.board)
-                    print(move.getChessNotation())
-                    gs.makeMove(move)
-                    sqSelected = ()
-                    playerClicks = []
-
-            drawGameState(screen, gs)
-            clock.tick(MAX_FPS)
-            p.display.flip()
+	p.init()
+	screen = p.display.set_mode((WIDTH, HEIGHT))
+	clock = p.time.Clock()
+	screen.fill(p.Color("white"))
+	gs = Engine.GameState()
+	validMoves = gs.getValidMoves()
+	moveMade = False
+	
+	loadImages()
+	running = True
+	sqSelected = ()
+	playerClicks = []
+	while running:
+		for i in p.event.get():
+			if i.type == p.QUIT:
+				running = False
+			elif i.type == p.MOUSEBUTTONDOWN:
+				location = p.mouse.get_pos()
+				col = location[0]//SQ_SIZE
+				row = location[1]//SQ_SIZE
+				if sqSelected == (row, col):
+					sqSelected = ()
+					playerClicks = []
+				else:
+					sqSelected = (row, col)
+					playerClicks.append(sqSelected)
+				if len(playerClicks) == 2:
+					move = Engine.Move(playerClicks[0], playerClicks[1], gs.board)
+					print(move.getChessNotation())
+					if move in validMoves:
+						gs.makeMove(move)
+						moveMade = True
+						sqSelected = ()
+						playerClicks = []
+					else:
+						playerClicks = [sqSelected]
+					
+			elif i.type == p.KEYDOWN:
+				if i.key == p.K_z:
+					gs.undoMove()
+					moveMade = True
+					
+		if moveMade:
+			validMoves = gs.getValidMoves()
+			moveMade = False
+			
+		drawGameState(screen, gs)
+		clock.tick(MAX_FPS)
+		p.display.flip()
 
 def drawGameState(screen, gs):
-    drawBoard(screen)
-    drawPieces(screen, gs.board)
+	drawBoard(screen)
+	drawPieces(screen, gs.board)
 
 def drawBoard(screen):
-    colors = [p.Color("#d8e3e7"), p.Color("#7396ac")]
-    for r in range (DIMENSION):
-        for e in range(DIMENSION):
-            color = colors[((r+e)%2)]
-            p.draw.rect(screen, color, p.Rect(e*SQ_SIZE, r*SQ_SIZE, SQ_SIZE, SQ_SIZE))
+	colors = [p.Color("#d8e3e7"), p.Color("#7396ac")]
+	for r in range (DIMENSION):
+		for e in range(DIMENSION):
+			color = colors[((r+e)%2)]
+			p.draw.rect(screen, color, p.Rect(e*SQ_SIZE, r*SQ_SIZE, SQ_SIZE, SQ_SIZE))
 
 
 def drawPieces(screen, board):
-    for r in range(DIMENSION):
-        for e in range(DIMENSION):
-            piece = board[r][e] 
-            if piece != "--":
-                screen.blit(IMAGES[piece], p.Rect(e*SQ_SIZE, r*SQ_SIZE, SQ_SIZE, SQ_SIZE))
+	for r in range(DIMENSION):
+		for e in range(DIMENSION):
+			piece = board[r][e] 
+			if piece != "--":
+				screen.blit(IMAGES[piece], p.Rect(e*SQ_SIZE, r*SQ_SIZE, SQ_SIZE, SQ_SIZE))
 
 if __name__ == "__main__":
-    main()
+	main()
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
